6b20a0e4c9dffa80b1058ae05fc315e7ad70f590
suraj7299 <surajbrnwal2424@gmail.com>
2025-04-12 19:46:14 +0530

    Added shadcn components and  middleware for Clerk authentication.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     
+    <ClerkProvider>
       <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-    
+      <Header />
+    </ClerkProvider>
   );
 }
