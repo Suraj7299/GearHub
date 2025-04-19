8078ebb725d2c7e09daae01625730ee166e04c07
suraj7299 <surajbrnwal2424@gmail.com>
2025-04-19 11:18:23 +0530

    feat: implement waitlist page with iframe and update next.config for CSP

--- app/waitlist/page.jsx
+++ app/waitlist/page.jsx
@@ -1,16 +1,12 @@
 "use client";
-
 import React from "react";
-import Head from "next/head";
+
 
 export default function WaitlistPage() {
   return (
     <>
-      <Head>
-        <title>Waitlist</title>
-      </Head>
-      <div className="mt-20 fixed w-full">
-        <div className="aspect-video w-full">Add iframe here</div>
+      <div>
+      <iframe style={{ width: "100%" }} height="569" src="https://gearhub-waitlist.created.app" title="Waiting List Landing Page" frameBorder="0"></iframe>
       </div>
     </>
   );
